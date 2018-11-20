66fd60653217cc0b0f3795b0cd0dda544187be62
Vincent Khougaz <vincent@khougaz.com>
2018-11-19 20:41:19 -0800

    SERVER_NAME->SHIFT_DOMAIN

--- legacy/fun/app/config.php
+++ legacy/fun/app/config.php
@@ -13,7 +13,7 @@ $DBTYPE = 'mysql';
 date_default_timezone_set('America/Los_Angeles');
 
 $PROTOCOL = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-$HOST = env_default('SERVER_NAME', $_SERVER['SERVER_NAME']);
+$HOST = env_default('SHIFT_DOMAIN', $_SERVER['SERVER_NAME']);
 $PATH = "/";
 $IMAGEDIR = "/opt/legacy/eventimages";
 $IMAGEURL = "/eventimages";
