0fcd582830b1b10f6f37863df15e0e776d6654de
Vincent Khougaz <vincent@khougaz.com>
2018-11-19 21:08:01 -0800

    Ensuring port is in email

--- legacy/fun/app/config.php
+++ legacy/fun/app/config.php
@@ -13,7 +13,8 @@ $DBTYPE = 'mysql';
 date_default_timezone_set('America/Los_Angeles');
 
 $PROTOCOL = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
-$HOST = env_default('SHIFT_DOMAIN', $_SERVER['SERVER_NAME']);
+$PORT = env_default('NGINX_HTTPS_PORT', '443');
+$HOST = env_default('SHIFT_DOMAIN', $_SERVER['SERVER_NAME']) . ($PORT !== "443" ? ":$PORT" : "");
 $PATH = "/";
 $IMAGEDIR = "/opt/legacy/eventimages";
 $IMAGEURL = "/eventimages";
