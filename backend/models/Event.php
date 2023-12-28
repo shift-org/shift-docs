174bdfca76bb0c479f1780eed89688b2bcde3bc4
Josh <josh@badmins-MacBook-Pro.local>
2023-12-27 17:25:40 -0800

    renamed function to clarify purpose; extracted some special text tokens as variables; moved secret URL to its own line to make it more obvious

--- backend/models/Event.php
+++ backend/models/Event.php
@@ -178,25 +178,41 @@ class Event extends fActiveRecord {
         $this->setPassword(md5(drupal_random_bytes(32)));
     }
 
-    public function emailSecret() {
+    public function sendConfirmationEmail() {
         global $PROTOCOL, $HOST, $PATH;
         $base = $PROTOCOL . $HOST . $PATH;
         $base = trim($base, '/'); // remove trailing slashes
 
+        $from_email = "bikefun@shift2bikes.org";
+        $support_email = "bikecal@shift2bikes.org";
+        $moderator_email = "shift-event-email-archives@googlegroups.com";
+
         $event_id = $this->getId();
         $secret = $this->getPassword();
-        $secret_url = "$base/addevent/edit-$event_id-$secret";
+        $secret_url = $base . "/addevent/edit-" . $event_id . "-" . $secret;
 
-        $headers = 'From: bikefun@shift2bikes.org' . "\r\n" .  'Reply-To: bikefun@shift2bikes.org' . "\r\n";
+        $headers = "From: " . $from_email . "\r\n";
+        $headers .= "Reply-To: " . $support_email . "\r\n";
+
         $subject = "Shift2Bikes Secret URL for " . $this->getTitle();
-        $message = "Dear " . $this->getName();
-        $message = $message . ", \r\n\r\nThank you for adding your event, " . $this->getTitle();
-        $message = $message . ", to the Shift Calendar. To activate the event listing, you must visit " . $secret_url . " and publish it.";
-        $message = $message . "\r\n\r\nThis link is like a password. Anyone who has it can delete and change your event. Please keep this email so you can manage your event in the future.";
-        $message = $message . "\r\n\r\nBike on!\r\n\r\n-Shift";
+
+        $blank_line .= "\r\n\r\n";
+        $message = "Dear " . $this->getName() . ", ";
+        $message .= $blank_line;
+        $message .= "Thank you for adding your event, " . $this->getTitle();
+        $message .= ", to the Shift Calendar. To activate the event listing, you must visit the confirmation link below and publish it:";
+        $message .= $blank_line;
+        $message .= $secret_url;
+        $message .= $blank_line;
+        $message .= "This link is like a password. Anyone who has it can delete and change your event. Please keep this email so you can manage your event in the future.";
+        $message .= $blank_line;
+        $message .= "Bike on!";
+        $message .= $blank_line;
+        $message .= "-Shift";
+
         mail($this->getEmail(), $subject, $message, $headers);
         # send backup copy for debugging and/or moderating
-        mail("shift-event-email-archives@googlegroups.com", $subject, $message, $headers);
+        mail($moderator_email, $subject, $message, $headers);
     }
 
     public function isPublished() {
