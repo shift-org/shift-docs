8cb5b814cd6d2583fae7b7de2efbfd94f6d48417
Josh <josh@badmins-MacBook-Pro.local>
2024-02-19 22:18:17 -0800

    removed comment

--- services/db/migrations/0005_char_set_and_collation.sql
+++ services/db/migrations/0005_char_set_and_collation.sql
@@ -2,5 +2,3 @@
 ALTER TABLE calevent CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 ALTER TABLE caldaily CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 ALTER DATABASE shift CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
--- will still need to update collation for MySQL v8 to utf8mb4_0900_ai_ci, 
--- but v5.7 doesn't recognize that collation
