dbaed817d643700e43770c6701d1dc2dfdc31b7d
Josh <josh@s0h091.luckylab.ptp>
2024-03-04 20:37:18 -0800

    removed unused compareDates()

--- site/themes/s2b_hugo_theme/static/js/cal/helpers.js
+++ site/themes/s2b_hugo_theme/static/js/cal/helpers.js
@@ -129,14 +129,4 @@ const DEFAULT_LENGTH = '--';
         return 0;
     };
 
-    $.fn.compareDates = function ( date1, date2 ) {
-        if ( date1 < date2 ) {
-            return -1;
-        }
-        if ( date1 > date2 ) {
-            return 1;
-        }
-        return 0;
-    };
-
 } (jQuery));
