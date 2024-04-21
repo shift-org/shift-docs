9813e820b3bf4d3967247cb6e861fc4f581b5fc4
ionous <github@ionous.net>
2024-04-21 15:09:01 -0700

    reworked the date picker
    - changes all date picker calcs to dayjs;  adds dayjs utc plugin
    - removes the load-earlier, load-later loading zones; associated css and js
    - simplifies checkBounds: uses a fixed size look-ahead ( of the same size as the  prev/next scroll steps ); removes the timer and timeout ( unclear why it was needed; relying on scroll events only seems more solid )
    - removes custom lists of month names; uses dayjs format instead
    - tweaks class building for improved? clarity ( i hope )
    - renames normalizeDate to toUtcString for clarity
    - removes selectedCount, doesn't seem to be needed
    - reverses "earliest" and "latest" month so that they match their meanings
    - adds comments

--- site/themes/s2b_hugo_theme/static/js/cal/datepicker.js
+++ site/themes/s2b_hugo_theme/static/js/cal/datepicker.js
@@ -1,74 +1,60 @@
 (function($) {
 
     // Global state variables, initialized in setupDatePicker
-    var $dateSelect,
-        $dateSelected,
-        $loadLater,
-        $loadEarlier,
-        $datePicker,
-        monthTemplate,
-        earliestMonth,
-        latestMonth,
-        dateMap,
-        today,
-        selectedCount = 0;
+    //-------------------
+    // the scrolling container of the grid of days
+    let $dateSelect;
+    // the grid of days minus the top and bottom "loading zones"
+    let $datePicker;
+    // the displayed list of user selected dates
+    let $dateSelected;
+    // html template from #mustache-select-month
+    let monthTemplate;
+    // these two start at the current month
+    // and grow to encompass the range of months displayed
+    // ( generated ) by the date picker.
+    let earliestMonth, latestMonth;
+    let dateMap; // maps utc YYYY-MM-DD -> { selected, dateStatus }
 
-    // Some constants used for generating html. The JOY of javascript stdlib
-    var monthNames = ["January", "February", "March", "April", "May", "June",
-        "July", "August", "September", "October", "November", "December"
-    ];
+    // scroll increment when pressing next/prev
+    // note: a week is about 28 units high..
+    const scrollStep = 112;
 
-    var cellClases = ["color-jan", "color-feb", "color-mar", "color-apr", "color-may", "color-jun",
-        "color-jul", "color-aug", "color-sep", "color-oct", "color-nov", "color-dec"];
-
+    // called by addevent.js
     $.fn.dateStatusesList = function() {
-        var selectedDates = $("#date-selected li").toArray();
-        var dateStatuses = selectedDates.map( function(date) {
-          var datestatus = {};
-          datestatus['id'] = date.getAttribute('data-id');
-          datestatus['date'] = date.querySelector('span').innerHTML;
-          datestatus['status'] = date.querySelector('select').value;
-          datestatus['newsflash'] = date.querySelector('.newsflash').value;
-          return datestatus;
+        const selectedDates = $("#date-selected li").toArray();
+        return selectedDates.map(date => {
+          return {
+            id: date.getAttribute('data-id'),
+            date:date.querySelector('span').innerHTML,
+            status: date.querySelector('select').value,
+            newsflash: date.querySelector('.newsflash').value,
+          };
         });
-        return dateStatuses;
     };
 
-
-    // setupDatePicker sets up the global variables and populates the date picker element
+    // sets up the global variables and populates the date picker element
     $.fn.setupDatePicker = function(dateStatuses) {
         // Fill in global variables
         // Set up dateMap
         dateMap = {};
         dateStatuses.forEach(function(dateStatus) {
-            dateMap[normalizeDate(dateStatus['date'])] = {
+            dateMap[toUtcString(dateStatus['date'])] = {
                 selected: true,
                 dateStatus: dateStatus
             }
-            selectedCount++;
         });
 
         // Scrolling container for the table
         $dateSelect = $('#date-select');
+        // the displayed list of user selected dates
         $dateSelected = $('#date-selected');
-        // Placeholder divs that trigger loading when visible
-        $loadLater = $('#load-later');
-        $loadEarlier = $('#load-earlier');
         // Table that contains months
         $datePicker = $("#date-picker");
-
+        // template for a single month
         monthTemplate = $('#mustache-select-month').html();
 
-        // Tracks when today is for styling purposes
-        today = new Date();
-        // These track which month to add to the start/end
-        earliestMonth = today;
-        latestMonth = today;
-
-        // Fill in the first month
-        $datePicker.html(getMonthHTML(earliestMonth));
-        // Scroll the table to the top of the first month
-        $dateSelect.scrollTop(loadEarlierBottom());
+        initDatePicker();
 
         // Add a click handler for individual days
         $datePicker.click(function(ev) {
@@ -80,17 +66,17 @@
                 if (date in dateMap) {
                     dateMap[date]['selected'] = !dateMap[date]['selected'];
                     if (dateMap[date]['selected']) {
-                        selectedCount++;
                         dateStatuses.push(dateMap[date]['dateStatus']);
                         $('.save-button').prop('disabled', false);
                         $('.preview-button').prop('disabled', false);
                     } else {
-                        selectedCount--;
                         var match = dateStatuses.findIndex(function(deselectedDate) {
                             return deselectedDate['date'] == date;
                         });
                         dateStatuses.splice(match, 1);
-                        if ( selectedCount === 0 ) {
+                        // fix? this doesn't disable the "publish" button
+                        // ex. if the user unselects all dates before publication.
+                        if ( !dateStatuses.length ) {
                             $('.save-button').prop('disabled', true);
                             $('.preview-button').prop('disabled', true);
                         }
@@ -107,7 +93,6 @@
                         selected: true,
                         dateStatus: newDateStatus
                     };
-                    selectedCount++;
                     $('.save-button').prop('disabled', false);
                     $('.preview-button').prop('disabled', false);
                 }
@@ -123,13 +108,13 @@
         // Setup the month table scroll checks
         $dateSelect.scroll(checkBounds);
         buildSortedDatesListHTML($dateSelected, dateStatuses);
-        checkBounds();
     };
 
+    // generate the "selected days" list
     function buildSortedDatesListHTML(list, dateStatuses) {
         dateStatuses.sort(function(a, b){
             // Sort dateStatuses in ascending order for display
-            return new Date(a['date']) - new Date(b['date']);
+            return dayjs(a.date).diff( dayjs(b.date) );
         }).forEach(function(dateStatus) {
             // Display null values as empty strings
             var dateStatusId = dateStatus['id'] ? dateStatus['id'] : "";
@@ -159,43 +144,53 @@
     }
 
     function isToday(date) {
-        return (date.getDate() === today.getDate()
-            && date.getMonth() === today.getMonth()
-            && date.getFullYear() === today.getFullYear())
+        return dayjs().isSame(date, 'day');
     }
 
-    function normalizeDate(date) {
-        var jsd = new Date(date);
-        jsd.setTime(jsd.getTime() + jsd.getTimezoneOffset()*60*1000);
-
-        return dayjs(jsd).format("YYYY-MM-DD");
+    // return the utc YYYY-MM-DD string of the passed date
+    // note: 'date' is sometimes a utc string, or a dayjs object.
+    // see also: main.js viewEvents()
+   function toUtcString(date) {
+        // tbd: does the server actually handle dates as utc?
+        // i believe times ( ex. start times ) are assumed local.
+        return dayjs(date).utc().format('YYYY-MM-DD');
     }
 
     function isSelected(date) {
-        return !!dateMap[normalizeDate(date)];
+        return !!dateMap[toUtcString(date)];
     }
 
-    function makeWeekData(date) {
-        // date is the first day in the week
+    // return an week of data for makeMonthData
+    // assumes that "start" is a sunday.
+    function makeWeekData(start) {
         // [{ day - string to display
         //    classes - classes to display },...]
-        var week = [];
-        for (var i=0;i<7;i++) {
-            var day = {};
-            day['day'] = date.getDate();
-            day['date'] = normalizeDate(date);
-            day['classes'] = (isToday(date) ? "today" : "") + " " + (isSelected(date) ? "selected" : "") + " " + cellClases[date.getMonth()] + (date.getDay() % 2 === 0 ? "-odd" : "");
-            day['today'] = isToday(date);
-            week.push(day);
-
-            date = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
+        let week = [];
+        for (let i=0; i<7; i++) {
+            const date = start.add(i, 'day');
+            const today = isToday(date);
+            const monthAbbr = date.format("MMM").toLowerCase(); // jan, dec
+            const classes = [
+              today ? "today" : "",
+              isSelected(date) ? "selected" : "",
+              // ex: color-apr-odd
+              // note: odd is actually placed on the even days.
+              "color-" + monthAbbr + ((i % 2) === 0 ? "-odd" : ""),
+            ];
+            //
+            week.push({
+              day: date.date(),        // day of the month ( as a number )
+              date: toUtcString(date), // date utc as a YYYY-MM-DD
+              classes: classes.join(" "),
+              today,
+            });
         }
         return week;
     }
 
+    // generate data describing the month containing the passed date.
+    // ( via the #mustache-select-month template in edit.html )
     function makeMonthData(date) {
-        // date is a day in a month to add
-
         // weeks:
         //   monthTitle - left title, only in first week
         //   weeksInMonth - rows that the title spans
@@ -203,85 +198,104 @@
         //     day: title
         //     date: yyyy-m-d (normalized)
         //     selected: cell class if selected
-
-        // Normalize date, ensuring first of month
-        var firstOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-        // Find the first day of the week for the week the 1st falls on
-        var firstDay = new Date(firstOfMonth.getFullYear(), firstOfMonth.getMonth(), -firstOfMonth.getDay()+1);
-
-        var firstOfNextMonth = new Date(date.getFullYear(), date.getMonth()+1, 1);
-        // Don't include the week next months 1st lands on
-        var stopDay = new Date(firstOfNextMonth.getFullYear(), firstOfNextMonth.getMonth(), -firstOfNextMonth.getDay()+1);
-
-        var weeks = [];
-        for (var startOfWeek=firstDay;startOfWeek<stopDay;) {
-            var week = {};
-            week['days'] = makeWeekData(startOfWeek);
-            weeks.push(week);
-
-            startOfWeek = new Date(startOfWeek.getFullYear(), startOfWeek.getMonth(), startOfWeek.getDate() + 7);
+        let weeks = [];
+        // the sunday before the first of the month ( can be the previous month )
+        const firstDay = date.startOf('month').startOf('week');
+        // the sunday before the first of *next* month ( can be this month )
+        const stopDay = date.add(1, 'month').startOf('month').startOf('week');
+        // stop *before* the week containing next month
+        // ( the next month will add that week as its first week. )
+        for (let d= firstDay; d.isBefore(stopDay, 'day'); d = d.add(1, 'week')) {
+            weeks.push({
+              days: makeWeekData(d) // an array of 7 days.
+            });
         }
-        weeks[0]['monthTitle'] = monthNames[firstOfMonth.getMonth()] + " " + firstOfMonth.getFullYear();
+        // the vertically oriented label on the side of the picker
+        weeks[0]['monthTitle'] = date.format("MMMM YYYY") // ex. January 2024
+        // indicates the height of the label: the number of rows (weeks) it should span
         weeks[0]['weeksInMonth'] = weeks.length;
-
-        return {"weeks": weeks};
+        return {weeks};
     }
+
+    // generate html for the month containing the passed date
+    // ( displayed by the date picker )
     function getMonthHTML(date) {
         return Mustache.render(monthTemplate, makeMonthData(date));
     }
 
-    function loadLaterTop() {
-        return $loadLater.offset().top - $dateSelect.offset().top;
-    }
-
-    function loadEarlierBottom() {
-        if ($loadEarlier) {
-            return ($loadEarlier.offset().top + $loadEarlier.prop('scrollHeight')) - $dateSelect.offset().top;
-        } else {
-            return 0;
-        }
-    }
-
-    var checking = false;
+    // add new html when needed.
+    // called from a scrolling event;
+    // ( the scrolling event can be initiated by next/prev/today buttons )
     function checkBounds() {
-        if (checking) {
-            return;
+        const grid = $dateSelect.get(0);
+        const currTop = grid.scrollTop;
+        // we're at the top; add some more events above.
+        if (currTop < scrollStep) {
+            const preHeight  = $datePicker.height();
+            earliestMonth = earliestMonth.subtract(1, 'month');
+            $datePicker.prepend(getMonthHTML(earliestMonth));
+
+            // try to keep the same position as before
+            const heightChange = $datePicker.height() - preHeight;
+            grid.scrollTop = currTop + heightChange;
         }
-        var added = false;
-        if (loadEarlierBottom() >= 0) {
-            latestMonth = new Date(latestMonth.getFullYear(), latestMonth.getMonth()-1, 1);
-            var preHeight = $datePicker.height();
-            $datePicker.prepend(getMonthHTML(latestMonth));
-            var heightChange = $datePicker.height() - preHeight;
-            $dateSelect.scrollTop($dateSelect.scrollTop() + heightChange);
-            added = true;
-        }
-        if (loadLaterTop() <= $dateSelect.height()) {
-            earliestMonth = new Date(earliestMonth.getFullYear(), earliestMonth.getMonth()+1, 1);
-            $datePicker.append(getMonthHTML(earliestMonth));
-            added = true;
-        }
-        if (added) {
-            checking = true;
-            setTimeout(function() {
-                checking = false;
-                checkBounds();
-            }, 10);
+        else {
+            // we're at the bottom: add some more events below.
+            // ( offset height is the fixed size element height )
+            // https://stackoverflow.com/questions/3962558/javascript-detect-scroll-end
+            const currBottom = currTop + grid.offsetHeight;
+            if (currBottom >= grid.scrollHeight) {
+                latestMonth = latestMonth.add(1, 'month');
+                $datePicker.append(getMonthHTML(latestMonth));
+            }
         }
     }
 
+    // generate the initial html
+    // fix: should this scroll the first scheduled day into view
+    // ( ex. when editing existing events )
+    function initDatePicker() {
+        const now = dayjs();
+        earliestMonth = now;
+        latestMonth = now.add(1, 'month');
+
+        // note: on chrome, if you prepend the earlier month here;
+        // the previous month winds up starting as the top month.
+        $datePicker.html(getMonthHTML(now));
+        $datePicker.append(getMonthHTML(latestMonth));
+        const grid = $dateSelect.get(0);
+        // however, we need to add a slight default offset
+        // otherwise, the top is at 0, and the user cant drag scroll upward
+        // as a side effect this triggers checkBounds
+        // and the previous month gets added anyway.
+        grid.scrollTop = 1;
+    }
+
+    // get the element on the datepicker containing today's date
+    function todaysElement() {
+      // tbd: can this use $datePicker?
+      return $("#date-picker .calendar-day.today")[0];
+    }
+
+    // the user has clicked the prev/left arrow button
+    // scroll to an earlier month.
     $(document).on('click', '#date-picker-prev-month', function(ev) {
-        var currentPosition = $("#date-select").scrollTop();
-        $("#date-select").scrollTop(currentPosition - 112);
+        const currentPosition = $dateSelect.scrollTop();
+        $dateSelect.scrollTop(currentPosition - scrollStep);
     });
 
+    // the user has clicked the next/right arrow button
+    // scroll to a later month.
     $(document).on('click', '#date-picker-next-month', function(ev) {
-        var currentPosition = $("#date-select").scrollTop();
-        $("#date-select").scrollTop(currentPosition + 112);
+        const currentPosition = $dateSelect.scrollTop();
+        $dateSelect.scrollTop(currentPosition + scrollStep);
     });
 
+    // the user has clicked the "today" button.
+    // scroll so that the current day is visible.
     $(document).on('click', '#date-picker-today', function(ev) {
-        $("#date-picker .calendar-day.today")[0].scrollIntoView({
+        const today = todaysElement();
+        today.scrollIntoView({
             block: "nearest",
             behavior: "smooth"
         });
