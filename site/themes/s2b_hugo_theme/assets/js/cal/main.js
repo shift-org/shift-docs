7df30389bb1f15bfe13d96c18072499835ada46a
Josh <josh@MacBook-Air.local>
2024-07-07 01:18:27 -0700

    moved max day range value to config; make day range requests inclusive of start & end dates

--- site/themes/s2b_hugo_theme/assets/js/cal/main.js
+++ site/themes/s2b_hugo_theme/assets/js/cal/main.js
@@ -103,7 +103,8 @@ $(document).ready(function() {
           // 'from' is today; for other PP pages it's options startdate.
           startdate: from,
           // if there was an enddate, use it; otherwise use a fixed number of days.
-          enddate: options.enddate ? end : from.add(dayRange, 'day'),
+          // subtract 1 so range is inclusive
+          enddate: options.enddate ? end : from.add(dayRange - 1, 'day'),
           // pass this on to the events listing.
           show_details: options.show_details,
         };
@@ -132,7 +133,7 @@ $(document).ready(function() {
                   .on('click', '#load-more', function(e) {
                       // the next day to view is one day after the previous last
                       view.startdate = view.enddate.add(1, 'day');
-                      view.enddate = view.startdate.add(dayRange, 'day');
+                      view.enddate = view.startdate.add(dayRange - 1, 'day');
                       // add new events to the end of those we've already added.
                       getEventHTML(view, function(eventHTML) {
                           $('#load-more').before(eventHTML);
