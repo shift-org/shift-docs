3bf8318619f8dd7fa5d04b54d4f5a407b16342bf
ionous <github@ionous.net>
2025-01-25 23:02:55 -0800

    avoid fetching data when subroutes change
    and simplify the date range request

--- site/static/js/cal/calList.js
+++ site/static/js/cal/calList.js
@@ -104,12 +104,14 @@ export default {
   template: `
 <div v-if="loading" class="loading">Loading...</div>
 <div v-if="error" class="error">{{ error }}</div>
+<section class="c-cal-list">
 <article v-for="day in days" 
   class="c-day" 
   :data-date="day.date">
   <h2>{{ longDate(day.date) }}</h2>
   <Event v-for="evt in day.events" :evt="evt"></Event>
 </article>
+</section>
 `,  
   data() {
     return {
@@ -128,12 +130,20 @@ export default {
       // (  i think because the route doesn't exist immediately )
       () => this.$route.query,
       // callback ( below )
-      this.fetchData,
+      this.queryChanged,
       // call when created ( as well as on changed )
       { immediate: true }
     )
   },
   methods: {
+    queryChanged(newq, oldq) {
+      const changed = !oldq || 
+      (newq.startdate  !== oldq.startdate) ||
+                      (newq.enddate  !== oldq.enddate);
+      if (changed) {
+        this.fetchData(newq);
+      }
+    },
     longDate(when) {
       const date = dayjs(when);
       const now = dayjs();
@@ -152,18 +162,32 @@ export default {
       }
       return date.format(format);
     },
-
-    async fetchData(query) {
+    // in the original, options came from events.html which read the hugo markdown
+    // pp pages would force the startdate to today
+    // ex. from pedalpalooza-calendar.md:
+    //   pp: true
+    //   startdate: 2024-06-01
+    //   enddate: 2024-08-31
+    // 
+    // enddate is optional ( might want to cap it to some reasonable amount )
+    async fetchData(q) {
       // read the days to view from the url.
-      const options = getInitialView(query);
-
+      // FIX: pedalp will have a fixed start and end
+      // but we want to move from "today"
+      //
+      const dayRange = 10;
+      const start = dayjs(q.startdate); // dayjs returns 'now' if startdate is missing.
+      const end = q.enddate ? dayjs(q.enddate) : start.add(dayRange, 'day');
+      const logFmt = "YYYY-MM-DD"
+      console.log(`fetching ${start.format(logFmt)} to ${end.format(logFmt)}`);
+      
       // FIX: lazyLoadEventImages
       // FIX: handler for #load-more
 
       this.error = this.post = null;
       this.loading = true;
       try {
-        const data= await dataPool.getRange(options.startdate, options.enddate);
+        const data= await dataPool.getRange(start, end);
         this.days = groupByDay(data);
         // console.log(JSON.stringify(data));
       } catch (err) {
@@ -172,31 +196,7 @@ export default {
         this.loading = false;
       }
     },
-  },
- 
-}
-
-// default range of days to show.
-const dayRange = 10;
-
-// compute range and details settings from the url options.
-// the returned object gets passed to getEventHTML().
-function getInitialView(options) {
-    const today = dayjs().startOf('day');
-    const start = dayjs(options.startdate); // if start or end are missing ( from the url )
-    const end   = dayjs(options.enddate);   // then dayjs returns today.
-    const inRange = today >= start && today <= end;
-    const from = (inRange && options.pp) ? today : start;
-    return {
-      // since this year's PP will be in range
-      // ( as will the normal calendar events page )
-      // 'from' is today; for other PP pages it's options startdate.
-      startdate: from,
-      // if there was an enddate, use it; otherwise use a fixed number of days.
-      enddate: options.enddate ? end : from.add(dayRange, 'day'),
-      // pass this on to the events listing.
-      show_details: options.show_details,
-    };
+  }
 }
 
 // takes the api events data and splits into an array of days:
