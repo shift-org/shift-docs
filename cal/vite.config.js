00b44b2bd6e9c3d6e9e73be9fbc64b9c66f65076
ionous <github@ionous.net>
2025-02-15 12:14:00 -0800

    experiment with netlify using npm run
    in theory netlify uses npm already; and this simplifies the toml a bit now that it also needs to run vite

--- cal/vite.config.js
+++ cal/vite.config.js
@@ -2,6 +2,17 @@ import path from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// use env to determine if this is being built by netlify.
+// https://docs.netlify.com/configure-builds/environment-variables/#build-metadata
+const isNetlify = !!process.env.NETLIFY;
+
+// the files in the extras directory are built by hugo.
+// ( for example: json for the menus, or pedalpalooza festival dates )
+// 'npm dev' tells hugo to output to the bin/dist directory;
+// while netlify always uses the site/public directory.
+const localExtras = "../bin/dist/extras/"
+const netlifyExtras = "../site/public/extras/"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
@@ -9,14 +20,8 @@ export default defineConfig({
   base: '/events',
   resolve: {
     alias: {
-      // used by siteConfig.js
-      // https://medium.com/@devxprite/how-to-setup-path-aliases-in-vite-df955939ffe8
-      extras: path.resolve(__dirname, "../bin/dist/extras/"),
+      // the extras are used by siteConfig.js
+      extras: path.resolve(__dirname, isNetlify? netlifyExtras: localExtras),
     },
-  },
-  build: {
-    // FIX: temp for testing
-    // really will need to integrate with netlify's build process.
-    outDir: '../bin/dist/events'
   }
 })
