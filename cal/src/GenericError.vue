17cd1a1add4c9f7502e56a084f20f167b68c25da
ionous <github@ionous.net>
2025-03-09 11:55:31 -0700

    improve error page
    ( better styles, adds home button )

--- cal/src/GenericError.vue
+++ cal/src/GenericError.vue
@@ -2,7 +2,9 @@
   Display a generic error.
  -->
 <script>
+import { RouterLink } from 'vue-router'
 export default {
+  components: { RouterLink },
   props: {
     error: [Object, String]
   },
@@ -15,14 +17,25 @@ export default {
 }
 </script>
 <template> 
-  <div class="c-error">{{errorMsg}}</div>
+  <div class="c-error">
+  <div class="c-error__msg">{{errorMsg}}</div>
+  <div class="c-error__home">
+    <RouterLink :to="{name:'calendar'}">Events Home</RouterLink>
+  </div>
+  </div>
 </template>
 <style>
 .c-error {
+  /*display: flex;
+  align-items: center;
+  flex-direction: column;*/
+  margin: 5em auto;
+  text-align: center;
+}
+.c-error__msg {
+  margin: 5em auto;
   font-size: 32px;
   background-color: firebrick;
   color: white;
-  margin: 5em auto;
-  text-align: center;
 }
 </style>
