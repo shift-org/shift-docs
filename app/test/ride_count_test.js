c7f4471e78fc209fa14173b9bc0e7964e862541c
ionous <github@ionous.net>
2025-10-09 17:40:26 -0700

    tests for the search endpoint
    tweaked the "limits" handling so that we never allow more than our default ( 25 ) at a time.

--- app/test/ride_count_test.js
+++ app/test/ride_count_test.js
@@ -6,7 +6,7 @@ const testdb = require("./testdb");
 chai.use(require('chai-http'));
 const expect = chai.expect;
 
-describe.only("ride count testing", () => {
+describe("ride count testing", () => {
   // runs before the first test in this block.
   before(function() {
     return testdb.setupWithFakeData();
---
+++ app/test/search_test.js
@@ -0,0 +1,86 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const app = require("../app");
+const testdb = require("./testdb");
+
+chai.use(require('chai-http'));
+const expect = chai.expect;
+
+describe.only("ride count testing", () => {
+  // runs before the first test in this block.
+  before(function() {
+    return testdb.setupWithFakeData();
+  });
+  // runs once after the last test in this block
+  after(function () {
+    return testdb.destroy();
+  });
+  // test:
+  it("errors on an empty search term", function(done) {
+    chai.request( app )
+      .get('/api/search.php')
+      // .query({q: "events"})
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(400);
+        expect(res).to.be.json;
+        expect(res.body).property('error').to.exist;
+        done();
+      });
+  });
+  it("handles a search", function(done) {
+    chai.request( app )
+      .get('/api/search.php')
+      .query({q: "go", all: true})
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.have.nested.property('pagination.fullcount', 8);
+        expect(res.body).property('events').lengthOf(8);
+        done();
+      });
+  });
+  it("handles narrow limits", function(done) {
+    chai.request( app )
+      .get('/api/search.php')
+      .query({
+          q: "go", 
+          l: 2,
+          all: true
+        })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.have.nested.property('pagination.fullcount', 8);
+        expect(res.body).to.have.nested.property('pagination.offset', 0);
+        expect(res.body).to.have.nested.property('pagination.limit', 2);
+        expect(res.body).property('events').lengthOf(2);
+        done();
+      });
+  });
+  it("handles offsets", function(done) {
+    chai.request( app )
+      .get('/api/search.php')
+      .query({
+          q: "go", 
+          o: 4,
+          l: 2,
+          all: true
+        })
+      .end(function (err, res) {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.have.nested.property('pagination.fullcount', 8);
+        expect(res.body).to.have.nested.property('pagination.offset', 4);
+        expect(res.body).to.have.nested.property('pagination.limit', 2);
+        expect(res.body).property('events').lengthOf(2);
+        const events = res.body.events;
+        expect(events[0].title).to.equal("I Can't Go For That (No Can Do)");
+        expect(events[1].title).to.equal("Na Na Hey Hey (Kiss Him Goodbye)");
+        done();
+      });
+  });
+});
